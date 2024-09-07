d376906b19235592472692e52cc6f86d56ac9c78
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-09-07 15:33:18 +0300

    uninstall gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-    },
-    base: command === 'build' && import.meta.env.VITE_DEPLOY_ENV === 'github' 
-      ? '/react-hw-01/' 
-      : '/',
-    build: {
-      outDir: 'dist'
-    }
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
   }
 })
